45c429a16ea8ae16cc961e0a3de8e61f3d0c98ab
Keigo Suzukawa <k.suzukawa@gmail.com>
2019-11-03 16:06:47 +0000

    Silence test code for now

--- src/canary/command.clj
+++ src/canary/command.clj
@@ -1,6 +1,7 @@
 (ns canary.command
   (:require [canary.db :as db]
-            [taoensso.faraday :as faraday]))
+            [taoensso.faraday :as faraday]
+            [clj-http.client :as client]))
 
 
 (defmulti handle first)
@@ -12,6 +13,19 @@
               :profile profile}]
    (faraday/put-item db/config :canary item)
    {}))
+
+
+(defmethod handle :authorise [[_ {:keys [code]}]]
+  #_(client/post "https://github.com/login/oauth/access_token"
+               {:body (slurp (muuntaja/encode "application/json"
+                                              {:client-id "1234"
+                                               :client-secret "1234"
+                                               :code code}))
+                :content-type :json
+                :accept :json})
+  ;; make connection
+  ;; create session
+  {:hi :there})
 
 
 (defmethod handle :default [command]
