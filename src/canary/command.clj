d10f6941eef32754aab3e6547057a9def5506892
Keigo Suzukawa <k.suzukawa@gmail.com>
2019-11-04 06:22:08 +0000

    Add a dummy user get

--- src/canary/command.clj
+++ src/canary/command.clj
@@ -20,14 +20,18 @@
   (let [request-body {"client_id" (System/getenv "AUTHORISATION_CLIENT_ID")
                       "client_secret" (System/getenv "AUTHORISATION_CLIENT_SECRET")
                       "code" code}
-        _ (println (slurp (muuntaja/encode "application/json" request-body)))
         response (client/post "https://github.com/login/oauth/access_token"
                               {:body (slurp (muuntaja/encode "application/json" request-body))
                                :content-type :json
-                               :accept :json})]
-    ;; make connection
+                               :accept :json})
+        user (client/get "https://api.github.com/user"
+                         {:headers {"Authorization" ["token" (get-in response [:body "access_token"])]}})]
+    ;; get user id
     ;; create session
-    {:github response}))
+    (println "************************************")
+    (println user)
+    (println "************************************")
+    {:hi :there}))
 
 
 (defmethod handle :default [command]
