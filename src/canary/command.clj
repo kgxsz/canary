49351895b4fafb8ac2aebbc8a686e3084a10a5f1
Keigo Suzukawa <k.suzukawa@gmail.com>
2019-11-05 20:34:12 +0000

    Add a profile to the db on authorisation

--- src/canary/command.clj
+++ src/canary/command.clj
@@ -9,32 +9,34 @@
 
 
 (defmethod handle :add-profile [[_ {:keys [user profile]}]]
- (let [item {:partition (str user ":profile")
+  (let [item {:partition (str user ":profile")
               :sort 123456
               :profile profile}]
-   (faraday/put-item db/config :canary item)
-   {}))
+    (faraday/put-item db/config :canary item)
+    {}))
 
 
 (defmethod handle :verify-authorisation [[_ {:keys [code]}]]
-  (let [request-body {"client_id" (System/getenv "AUTHORISATION_CLIENT_ID")
-                      "client_secret" (System/getenv "AUTHORISATION_CLIENT_SECRET")
-                      "code" code}
-        response (client/post "https://github.com/login/oauth/access_token"
-                              {:body (slurp (muuntaja/encode "application/json" request-body))
-                               :content-type :json
-                               :accept :json})
-        access-token (:access_token (muuntaja/decode "application/json" (:body response)))
-        response (client/get "https://api.github.com/user"
-                             {:headers {:authorization (format "token %s" access-token)}})
-        user (muuntaja/decode "application/json" (:body response))]
-    ;; get user id
-    ;; create session
-    (println "************************************")
-    (println user)
-    (println (keys user))
-    (println "************************************")
-    {:hi user}))
+  (let [request-body {:client_id (System/getenv "AUTHORISATION_CLIENT_ID")
+                      :client_secret (System/getenv "AUTHORISATION_CLIENT_SECRET")
+                      :code code}
+        {:keys [body]} (client/post
+                        "https://github.com/login/oauth/access_token"
+                        {:body (slurp (muuntaja/encode "application/json" request-body))
+                         :content-type :json
+                         :accept :json})
+        access-token (:access_token (muuntaja/decode "application/json" body))
+        {:keys [body]} (client/get
+                        "https://api.github.com/user"
+                        {:headers {:authorization (format "token %s" access-token)}})
+        user (muuntaja/decode "application/json" body)
+        item {:partition (str (:login user) ":profile")
+              :sort 1234567
+              :profile {:id (:id user)
+                        :username (:login user)
+                        :created-at 1234567}}]
+    (faraday/put-item db/config :canary item)
+    {}))
 
 
 (defmethod handle :default [command]
